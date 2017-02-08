5daae29bee944e30ff6539d7fa2c285af8c6bf51
Jigar <jigarmistry@JigarMs-Mac-mini.local>
2017-02-08 10:37:59 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import configparser
+from selenium import webdriver
+
+browser = webdriver.PhantomJS(
+    "/usr/local/lib/phantomjs/bin/phantomjs",
+    service_args=[
+        '--ssl-protocol=any', '--ignore-ssl-errors=true', '--load-images=no'
+    ])
+
+url_paw = "https://www.pythonanywhere.com/login/"
+
+
+def get_credentials():
+
+    config = configparser.ConfigParser()
+    config.read('credentials.ini')
+    sites = config.sections()
+    dictCreds = {}
+
+    if len(sites) == 0:
+        return dictCreds, False
+
+    for site in sites:
+        dictCreds[site] = {}
+        dictCreds[site]["username"] = config[site]["username"]
+        dictCreds[site]["password"] = config[site]["password"]
+    return dictCreds, True
+
+
+def do_login(site, data):
+
+    try:
+        browser.get(url_paw)
+        userNameElem = browser.find_element_by_id('id_auth-username')
+        userNameElem.send_keys(data["username"])
+        passwordElem = browser.find_element_by_id('id_auth-password')
+        passwordElem.send_keys(data["password"])
+        logInBtnElem = browser.find_element_by_id('id_next')
+        logInBtnElem.click()
+        try:
+            webLinkElem = browser.find_element_by_id('id_web_app_link')
+            webLinkElem.click()
+            appExtendElem = browser.find_element_by_css_selector(
+                '.btn.btn-warning.webapp_extend')
+            appExtendElem.click()
+            expiryNoteElem = browser.find_element_by_class_name(
+                'webapp_expiry')
+            print(site + " : " + expiryNoteElem.text)
+        except Exception as e:
+            print("Credentials are wrong for " + site)
+    except Exception as e:
+        print("Something went wrong for " + site)
+
+
+if __name__ == "__main__":
+    creds, st = get_credentials()
+    if st:
+        for site, value in creds.items():
+            do_login(site, value)
+    else:
+        print("No Credentials Data Found")
